eeac3daac26465648fcf280f1dfbabba2a07720e
Sarah Rocco <sarahrocco90@gmail.com>
2025-04-05 22:04:44 +0200

    get rid of random shapes and improve code so walls adjust based on set canvas

--- script.js
+++ script.js
@@ -20,23 +20,9 @@ Runner.run(Runner.create(), engine);
 
 // Walls
 const walls = [
-    Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
-    Bodies.rectangle(400, 600, 800, 40, { isStatic: true}),
-    Bodies.rectangle(0, 300, 40, 600, { isStatic: true}),
-    Bodies.rectangle(800, 300, 40, 600, { isStatic: true})
+    Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+    Bodies.rectangle(width /2, height, width, 40, { isStatic: true}),
+    Bodies.rectangle(0, height / 2, 40, height, { isStatic: true}),
+    Bodies.rectangle(width, height / 2, 40, height, { isStatic: true})
 ];
 World.add(world, walls);
-
-// Random Shapes
-for (let i = 0; i < 50; i++ ){
-    if (Math.random() > 0.5) {
-World.add(
-    world,
-    Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50));
-} else {
-    World.add(
-        world,
-        Bodies.circle(Math.random() * width, Math.random() * height, 35)
-    );
-} 
-}

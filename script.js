f61c6303141154746668504797aa50ef3c7efa59
Sarah Rocco <sarahrocco90@gmail.com>
2025-04-08 21:12:51 +0200

    create movement direction

--- script.js
+++ script.js
@@ -72,21 +72,30 @@ const stepThroughCell = (row, column) => {
 
     // Assemble randomly-ordered list of neighbors
     const neighbors = shuffle([
-        [row - 1, column],
-        [row, column + 1]
-        [row + 1, column]
-        [row, column - 1]
+        [row - 1, column 'up'],
+        [row, column + 1, 'right'],
+        [row + 1, column, 'down'],
+        [row, column - 1, 'left']
     ]);
 
     // for each neighbor...
 
-    
+    for (let neighbor of neighbors) {
+        const [nextRow, nextColumn, direction] = neighbor;
 
     // See if that neighbor is out of bounds
-
+    if (nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) {
+      continue;  
+    }
+    
     // if we have visited that neighbor, continue to next neighbor
+    if (grid[nextRow][nextColumn]) {
+        continue;
+    }
 
     // Remove a wall from either horizontals or verticals
+    
+    }
 
     // Visit that next cell
 };
